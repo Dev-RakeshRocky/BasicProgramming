ff2716c705de81d12f6ca8eebab526c14983f0b4
Rakesh <vjtjrakesh@gmail.com>
2020-09-16 08:14:27 +0530

    Length of last word

---
+++ src/com/StringsAndArrays/LengthOfLastWord.java
@@ -0,0 +1,20 @@
+package com.StringsAndArrays;
+//Prob number 58 Easy
+public class LengthOfLastWord {
+	private static int calcluateLength(String s) {
+		int result =0;
+		int n =s.length();
+		while(n>0) {
+			if(s.charAt(--n)!=' ')
+				result++;
+			else if(result>0) 
+				return result;
+		}
+	return result;
+	}
+	public static void main(String[] args) {
+       String word = "Hello WORLD";
+       System.out.println( calcluateLength(word));    
+	}
+
+}
