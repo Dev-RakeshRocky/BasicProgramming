724561a3afdbbc200ae21ea95bc78a21bce6cd6f
Rakesh <vjtjrakesh@gmail.com>
2020-10-20 20:42:37 +0530

    Maximum sub Array

---
+++ src/com/StringsAndArrays/MaximumSubArray.java
@@ -0,0 +1,28 @@
+package com.StringsAndArrays;
+
+/*
+   Given an integer array nums, find the contiguous subarray (containing at least one number)
+   which has the largest sum and return its sum.
+   Follow up: If you have figured out the O(n) solution, 
+   try coding another solution using the divide and conquer approach, which is more subtle.
+    Input: nums = [-2,1,-3,4,-1,2,1,-5,4]
+    Output: 6
+    Explanation: [4,-1,2,1] has the largest sum = 6.
+*/
+public class MaximumSubArray {
+	private static Integer maxSubArray(Integer[] nums) {
+		Integer result = 0;
+		int[] sum = new int[nums.length];
+		sum[0] =nums[0];
+		for(int i=1;i<nums.length;i++)
+		{
+		sum[i]=Math.max(nums[i], sum[i-1]+nums[i]);
+		result = Math.max(result,sum[i]);
+		}
+		return result;
+	}
+	public static void main(String[] args) {
+		Integer[] in = {-2,1,-3,4,-1,2,1,-5,4};
+        System.out.println(maxSubArray(in));
+	}
+}
